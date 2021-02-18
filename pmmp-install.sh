c37685287750e522143a8175c2bf44064eec7b6a
Khuirul Huda <67778682+Khuirul-Huda@users.noreply.github.com>
2021-02-18 20:45:23 +0700

    Add files via upload

--- pmmp-install.sh
+++ pmmp-install.sh
@@ -15,7 +15,7 @@ if [ $answers == y ]; then
 sudo apt update
 sudo apt upgrade -y 
 fi
-
+SECONDS = 0
 sudo apt install tar
 #start pmmp install
 
@@ -32,5 +32,20 @@ if [ $start == y ]; then
 
 ./start.sh
 fi
-echo "To Start Server -> ./start.sh"
+echo "To Start Server, Type ./start.sh"
+echo "dont forget to use screen so the server will always on!"
 echo "ThankYou"
+
+
+if (( $SECONDS > 3600 )) ; then
+    let "hours=SECONDS/3600"
+    let "minutes=(SECONDS%3600)/60"
+    let "seconds=(SECONDS%3600)%60"
+    echo "Installation completed in $hours hour(s), $minutes minute(s) and $seconds second(s)" 
+elif (( $SECONDS > 60 )) ; then
+    let "minutes=(SECONDS%3600)/60"
+    let "seconds=(SECONDS%3600)%60"
+    echo "Installation completed in $minutes minute(s) and $seconds second(s)"
+else
+    echo "Installation completed in $SECONDS seconds"
+fi
