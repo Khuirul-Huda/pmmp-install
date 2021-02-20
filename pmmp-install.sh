2c30c8f0763f8858ab9223310274435e45859ef0
Khuirul-Huda <hudah2102@gmail.com>
2021-02-20 12:15:13 +0700

    Add installation on termux

--- pmmp-install.sh
+++ pmmp-install.sh
@@ -7,5 +7,24 @@ if [ $answers == y ]; then
 sudo apt update
 sudo apt upgrade -y 
 fi
-
+echo "PMMP INSTALL [PREPARE]"
+echo "================="
+echo "Installation on:"
+echo "[1] Linux (Ubuntu, etc.)"
+echo "[2] Termux (Android)"
+echo "=================="
+echo " Answers: (1/2)"
+read installon
+if [ $installon == 1 ]; then
 bash <(curl https://raw.githubusercontent.com/Khuirul-Huda/pmmp-install/main/execute.sh)
+echo "Selected [1] Linux"
+elif [$installon == 2]; then
+  bash <(curl https://raw.githubusercontent.com/Khuirul-Huda/pmmp-install/main/termux.sh)
+  echo "Selected [2] Termux"
+  pkg update
+  pkg upgrade
+  pkg install wget
+  pkg install curl
+ 
+fi
+echo "Installation Prepare End"
---
+++ termux.sh
@@ -0,0 +1,29 @@
+SECONDS=0
+pkg install tar
+#start pmmp install
+
+wget "https://jenkins.pmmp.io/job/PHP-7.4-Linux-x86_64/9/artifact/PHP_Linux-x86_64.tar.gz"
+tar -xvf "PHP_Linux-x86_64.tar.gz"
+rm "PHP_Linux-x86_64.tar.gz"
+wget "https://jenkins.pmmp.io/job/PocketMine-MP/1828/artifact/PocketMine-MP.phar"
+wget "https://jenkins.pmmp.io/job/PocketMine-MP/1828/artifact/start.sh"
+chmod +x ./start.sh
+#start the server for first time
+
+echo "To Start Server, Type ./start.sh"
+echo "dont forget to use screen so the server will always on!"
+echo "ThankYou"
+
+
+if (( $SECONDS > 3600 )) ; then
+    let "hours=SECONDS/3600"
+    let "minutes=(SECONDS%3600)/60"
+    let "seconds=(SECONDS%3600)%60"
+    echo "Installation completed in $hours hour(s), $minutes minute(s) and $seconds second(s)" 
+elif (( $SECONDS > 60 )) ; then
+    let "minutes=(SECONDS%3600)/60"
+    let "seconds=(SECONDS%3600)%60"
+    echo "Installation completed in $minutes minute(s) and $seconds second(s)"
+else
+    echo "Installation completed in $SECONDS seconds"
+fi
